14ceb483e5ced1c21f23eff327c537ca163d69e5
Jesse <Jdorman1289@gmail.com>
2023-08-30 09:24:01 -0500

    finish quote and author beans

---
+++ src/main/java/Author.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Author {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private ArrayList<Quote> quotes;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, ArrayList<Quote> quotes) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.quotes = quotes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Quote> getQuotes() {
+        return quotes;
+    }
+
+    public void setQuotes(ArrayList<Quote> quotes) {
+        this.quotes = quotes;
+    }
+
+    public void addQuote(Quote quote) {
+        this.quotes.add(quote);
+    }
+}
