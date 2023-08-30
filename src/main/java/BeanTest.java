49f6a768b407fb90e6eb2f491b534c1ae72373b6
Jesse <Jdorman1289@gmail.com>
2023-08-30 09:29:15 -0500

    create main method to brew beans

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,7 @@
+public class BeanTest {
+    public static void main(String[] args) {
+        Album album = new Album("Biscuit", "Gravy",2023,100.50,"Delicious");
+        System.out.println(album.getArtist());
+
+    }
+}
